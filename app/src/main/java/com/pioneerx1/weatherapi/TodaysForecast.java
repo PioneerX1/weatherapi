f048f5c7f06f64a75974f1bd7047ad0b59166ca8
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-12 14:54:14 -0700

    add code to test api, no user output yet, just logcat

--- app/src/main/java/com/pioneerx1/weatherapi/TodaysForecast.java
+++ app/src/main/java/com/pioneerx1/weatherapi/TodaysForecast.java
@@ -3,16 +3,51 @@ package com.pioneerx1.weatherapi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.IOException;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class TodaysForecast extends AppCompatActivity {
+
+    public static final String TAG = TodaysForecast.class.getSimpleName();
+
     @Bind(R.id.textViewLocation2) TextView mTextViewLocation2;
+
+    Intent oldIntent = getIntent();
+    final String location = oldIntent.getStringExtra("location");
+
+    private void getWeather(String location) {
+        final OpenWeatherService openWeatherService = new OpenWeatherService();
+        openWeatherService.findWeather(location, new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +55,8 @@ public class TodaysForecast extends AppCompatActivity {
         setContentView(R.layout.activity_todays_forecast);
         ButterKnife.bind(this);
 
-        Intent oldIntent = getIntent();
-        final String location = oldIntent.getStringExtra("location");
         mTextViewLocation2.setText("You entered " + location);
+        getWeather(location);
 
     }
 }
