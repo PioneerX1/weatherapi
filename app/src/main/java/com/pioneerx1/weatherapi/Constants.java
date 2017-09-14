2d1b0b8e002e1d33deb70dcae787fa2bde64321b
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-14 08:54:51 -0700

    add adjustments to gradle properties, constants, and build gradle files for open weather api key info

--- app/src/main/java/com/pioneerx1/weatherapi/Constants.java
+++ app/src/main/java/com/pioneerx1/weatherapi/Constants.java
@@ -2,9 +2,10 @@ package com.pioneerx1.weatherapi;
 
 
 public class Constants {
-    public static final String API_KEY = "&APPID=c109379d321012346a8b0e6bdea99879";
+    public static final String API_KEY = "BuildConfig.API_KEY";
     public static final String API_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?id=";
     public static final String LOCATION_QUERY_PARAMETER = "location"; //Example: "location"
+    public static final String API_KEY_QUERY_PARAMETER = "appid";
     // public static final String API_KEY_QUERY_PARAMETER = "appid";
     // connect like: API_BASE_URL + YOUR_QUERY_PARAMETER + API_KEY
 }
